2750efa2248a90830501e6e04a55021f68722f02
CorreaBrunoMiguel <devsistemascorrea@gmail.com>
2024-03-25 19:14:50 -0300

    Projeto Inicializado

---
+++ src/main/java/br/com/fullstack/m1s9/ExerciciosM1S9Application.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.m1s9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosM1S9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciciosM1S9Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fullstack/m1s9/ExerciciosM1S9ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.m1s9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosM1S9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
